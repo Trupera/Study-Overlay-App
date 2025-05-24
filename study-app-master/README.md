97d08d13c5f99a1cae8a1937af426c1a21d224be
Trupera <66578152+Trupera@users.noreply.github.com>
2025-05-23 23:27:53 -0400

    beta source code

---
+++ study-app-master/README.md
@@ -0,0 +1,7 @@
+Developer: Trupera
+Build Date: 5/23/25
+
+Version: Alpha 1.0
+
+This is the overwolf app that will automate study sessions while you game.
+Now you can prepare for your test while you climb ranks!
---
+++ study-app-master/native/windows/desktop/desktop.js
@@ -0,0 +1,10 @@
+import { DesktopController } from './desktop-controller.js';
+
+const desktopController = new DesktopController();
+
+
+// store the value of the inputted website here then export it
+
+
+desktopController.run();
+
